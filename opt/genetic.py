388847e667d82d8661da88c4f9f0a20797a834b9
Piotr Sobecki <ptrsbck@gmail.com>
2017-03-25 20:37:40 +0100

    log helper

--- opt/genetic.py
+++ opt/genetic.py
@@ -2,7 +2,62 @@ import logging
 import json, csv
 import pandas as pd
 from deap import creator, base, tools, algorithms
-from opt.base import Configuration, Optimizer,Results
+from opt.base import Configuration, Optimizer, Results
+
+#TODO MOVE DATALOG SOMEWHERE LOWER
+class GeneticLogHelper():
+    def __init__(self, genlog, datalog, sep):
+        self.genlog = genlog
+        self.datalog = datalog
+        self.sep = sep
+
+    def get_genlog(self):
+        return pd.read_csv(self.genlog, self.sep, index_col=0)
+
+    def get_datalog(self):
+        return pd.read_csv(self.datalog, self.sep, index_col=0)
+
+    def write_row_2file(self, row, csv_writer, file):
+        csv_writer.writerow(row)
+        file.flush()
+
+    def log(self, context, generation_no, results):
+        self.log_generation(context, generation_no, results)
+        self.log_configuration(context, generation_no, results)
+
+    def log_generation(self, context, generation_no, results):
+        gen_row = [generation_no]
+        gen_row.extend(results.fitness())
+        self.write_row_2file(gen_row, context['csv_gen'], context['csv_gen_file'])
+
+    def log_configuration(self, context, generation_no, results):
+        max_config = results.max()
+        row = [generation_no, max_config.value()]
+        row.extend(max_config.as_list())
+        self.write_row_2file(row, context['csv'], context['csv_file'])
+
+    def setup_genlog(self, context):
+        gencols = ['Generation']
+        gencols.extend(['#' + str(x) for x in range(0, context.settings['n'])])
+        context['csv_gen_file'] = open(self.genlog, 'a+')
+        context['csv_gen'] = csv.writer(context['csv_gen_file'], delimiter=';', lineterminator='\n')
+        self.write_row_2file(gencols, context['csv_gen'], context['csv_gen_file'])
+
+    def setup_configuration_log(self, context):
+        cols = ['Generation', 'Max Fitness']
+        cols.extend(context.features.columns.tolist())
+        context['csv_file'] = open(self.datalog, 'a+')
+        context['csv'] = csv.writer(context['csv_file'], delimiter=';', lineterminator='\n')
+        self.write_row_2file(cols, context['csv'], context['csv_file'])
+
+    def setup(self, context):
+        self.setup_configuration_log(context)
+        self.setup_genlog(context)
+
+    def close(self, context):
+        context['csv_file'].close()
+        context['csv_gen_file'].close()
+
 
 
 class RoutingHOF():
@@ -22,6 +77,7 @@ class RoutingHOF():
         self.ngen += 1
         self.results = results
 
+
 class GeneticConfiguration(Configuration):
     def value(self):
         return self.individual.fitness.values[0]
@@ -29,7 +85,6 @@ class GeneticConfiguration(Configuration):
 
 # noinspection PyTypeChecker,PyUnresolvedReferences
 class GeneticOptimizer(Optimizer):
-
     results_class = Results
 
     def __init__(self, **settings):
@@ -84,50 +139,18 @@ class GeneticOptimizer(Optimizer):
             return self.settings[item]
         return None
 
-    def write_row_2file(self, row, csv_writer, file):
-        csv_writer.writerow(row)
-        file.flush()
-
     def on_gen_end(self, context, generation_no, results):
         self.logger.debug('Generation %d, max: %s' % (generation_no, results.max()))
         if self.fileverbose:
-            self.log_configuration(context, generation_no, results)
-            self.log_generation(context, generation_no, results)
+            context.log.log(context, generation_no, results)
 
     def on_fit_start(self, context):
         if self.fileverbose:
-            self.setup_configuration_log(context)
-            self.setup_genlog(context)
+            context.log.setup(context)
 
     def on_fit_end(self, context):
         if self.fileverbose:
-            context['csv_file'].close()
-            context['csv_gen_file'].close()
-
-    def log_generation(self, context, generation_no, results):
-        gen_row = [generation_no]
-        gen_row.extend(results.fitness())
-        self.write_row_2file(gen_row, context['csv_gen'], context['csv_gen_file'])
-
-    def log_configuration(self, context, generation_no, results):
-        max_config = results.max()
-        row = [generation_no, max_config.value()]
-        row.extend(max_config.as_list())
-        self.write_row_2file(row, context['csv'], context['csv_file'])
-
-    def setup_configuration_log(self,context):
-        cols = ['Generation', 'Max Fitness']
-        cols.extend(self.features.columns.tolist())
-        context['csv_file'] = open(self.datalog, 'a+')
-        context['csv'] = csv.writer(context['csv_file'], delimiter=';', lineterminator='\n')
-        self.write_row_2file(cols, context['csv'], context['csv_file'])
-
-    def setup_genlog(self,context):
-        gencols = ['Generation']
-        gencols.extend(['#' + str(x) for x in range(0, self.n)])
-        context['csv_gen_file'] = open(self.genlog, 'a+')
-        context['csv_gen'] = csv.writer(context['csv_gen_file'], delimiter=';', lineterminator='\n')
-        self.write_row_2file(gencols, context['csv_gen'], context['csv_gen_file'])
+            context.log.close(context)
 
     def fit(self):
         self.logger.info(self.settings)
@@ -140,6 +163,9 @@ class GeneticOptimizer(Optimizer):
         self.select(toolbox)
         population = toolbox.population(self.n)
         context = {}
+        context.settings = self.settings
+        context.features = self.features
+        context.log = GeneticLogHelper(**self.settings)
         self.on_fit_start(context)
         hof = RoutingHOF(self, context, results_class=self.results_class)
         algorithms.eaSimple(population, toolbox, cxpb=self.cxpb, mutpb=self.mutpb, ngen=self.ngen, halloffame=hof,
