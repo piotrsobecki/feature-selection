ac60997eb36388bdc903b9741f59cdcacbeff452
Piotr Sobecki <ptrsbck@gmail.com>
2017-05-10 01:02:26 +0200

    ismis dynamic model configuration

--- tests/classifiers/ismis17_prostatex.py
+++ tests/classifiers/ismis17_prostatex.py
@@ -26,11 +26,7 @@ def crossvalidation(clf, X, Y, K=3):
  return prostatex_auc(Y, y_proba)
 
 
-# Weighted
-
-
-
-X = pandas.DataFrame.from_csv("tests/data/PROSTATEx/features.csv", sep=";", index_col=None)
+X = pandas.DataFrame.from_csv("tests/data/PROSTATEx/all/features.csv", sep=";", index_col=None)
 Y = X.loc[:, 'clinsig'].values
 del X['clinsig']
 
@@ -53,6 +49,14 @@ KTRANS.extend(LOCATION)
 
 n_neighbors=7
 
+T2_ALL_ATTRS = [*T2_COR, *T2_SAG, *T2_TRA]
+T2_TRA_ADC_ATTRS = [*T2_TRA, *DWI_ADC]
+DWI_KTRANS_ATTRS = [*DWI_ADC,*KTRANS]
+T2_DWI_KTRANS_ATTRS = [*T2_ALL_ATTRS,*DWI_ADC,*KTRANS]
+T2_DWI_ATTRS = [*T2_ALL_ATTRS,*DWI_ADC]
+T2_KTRANS_ATTRS = [*T2_ALL_ATTRS,*KTRANS]
+ALL_ATTRS = [*T2_COR,*T2_SAG,*T2_TRA,*DWI_ADC,*KTRANS]
+
 def test(name, alpha, beta, weight, method='predict_proba'):
     t2_cls = ProbabalisticClassifier(KNeighborsClassifier(n_neighbors=n_neighbors), method=method, alpha=alpha, beta=beta,  weight=weight, columns=T2_ALL)
     t2cor_cls = ProbabalisticClassifier(KNeighborsClassifier(n_neighbors=n_neighbors), method=method, alpha=alpha, beta=beta,  weight=weight, columns=T2_COR)
@@ -67,7 +71,7 @@ def test(name, alpha, beta, weight, method='predict_proba'):
 
         cls_ensemble = WeightedEnsemble(cls)
         score = crossvalidation(cls_ensemble, X, Y, 3)
-        print('%s; %s ;%0.2f;%0.2f;%0.2f; %0.2f ' % (name,subname,alpha,beta,weight,score))
+        print('%s; %s ;%0.6f;%0.6f;%0.6f; %0.6f ' % (name,subname,alpha,beta,weight,score))
 
     test_ensemble("ALL",[t2cor_cls, t2sag_cls, t2tra_cls, dwiadc_cls, ktrans_cls])
     test_ensemble("T2-TRA+ADC",[t2tra_cls, dwiadc_cls])
@@ -80,23 +84,36 @@ def test(name, alpha, beta, weight, method='predict_proba'):
 
 
 # Weighted
-print("Configuration;Version;Alpha;Beta;Weight;AUC")
-test("Probabalistic Ensemble",   0,   1, None)
-test("Probabalistic Ensemble", .50, .50, None)
-test("Probabalistic Ensemble", .25, .75, None)
-test("Probabalistic Ensemble", .75, .25, None)
+#print("Configuration;Version;Alpha;Beta;Weight;AUC")
+#test("Probabalistic Ensemble",   0,   1, None)
+#test("Probabalistic Ensemble", .50, .50, None)
+#test("Probabalistic Ensemble", .25, .75, None)
+#test("Probabalistic Ensemble", .75, .25, None)
+#
+## UnWeighted
+#test("Probabalistic Ensemble",   0,   1, 1)
+#test("Probabalistic Ensemble", .50, .50, 1)
+#test("Probabalistic Ensemble", .25, .75, 1)
+#test("Probabalistic Ensemble", .75, .25, 1)
+#
+## VOTING
+#test("Voting Ensemble", 1, 0, None)
+#test("Voting Ensemble", 1, 0, 1)
 
-# UnWeighted
-test("Probabalistic Ensemble",   0,   1, 1)
-test("Probabalistic Ensemble", .50, .50, 1)
-test("Probabalistic Ensemble", .25, .75, 1)
-test("Probabalistic Ensemble", .75, .25, 1)
-
-# VOTING
-test("Voting Ensemble", 1, 0, None)
-test("Voting Ensemble", 1, 0, 1)
 
 
-score = crossvalidation(KNeighborsClassifier(n_neighbors=n_neighbors), X, Y, 3)
+def test_knn(attrs,name):
+    print('%s;%0.6f' % (name,crossvalidation(KNeighborsClassifier(n_neighbors=n_neighbors), X.loc[:,attrs], Y, 3)))
 
-print('KNN - Accuracy: %0.2f ' % ( score))
+test_knn(ALL_ATTRS,"ALL")
+test_knn(T2_TRA_ADC_ATTRS,"T2-TRA+ADC")
+test_knn(T2_ALL_ATTRS,"T2")
+test_knn(DWI_KTRANS_ATTRS,"DWI+KTRANS")
+test_knn(T2_KTRANS_ATTRS,"T2+KTRANS")
+test_knn(T2_DWI_KTRANS_ATTRS,"T2+DWI+KTRANS")
+test_knn(T2_DWI_ATTRS,"T2+DWI")
+test_knn(T2_COR,"T2-COR")
+test_knn(T2_SAG,"T2-SAG")
+test_knn(T2_TRA,"T2-TRA")
+test_knn(DWI_ADC,"ADC")
+test_knn(KTRANS,"KTRANS")
